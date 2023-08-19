6275f9c311329c7205083e13dce9aff0e09ae3ae
mikgit <mikelhfzy@gmail.com>
2023-08-19 20:15:04 +0300

    finishing

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import './App.css'
+import React, { useState } from 'react';
+import data from './data.json';
+import Notification from './components/notification/Notification';
+
+function App() {
+
+  const [notificationData, setNotificationData] = useState(data);
+  const handleNotificationClick = (index) => {
+    const updatedData = [...notificationData];
+    updatedData[index].status = 'read'; // Update the status to 'read' or any other value as needed
+    setNotificationData(updatedData);
+  };
+
+  const markAllAsRead = () => {
+    const updatedData = notificationData.map((notification) => {
+      return {
+        ...notification,
+        status: 'read',
+      };
+    });
+    setNotificationData(updatedData);
+  };
+
+  return (
+    <>
+      <main>
+        <div className="head">
+          <div className="notifiy">
+            <h2>Notifications <span className="number">3</span></h2>
+          </div>
+
+          <div className="button" onClick={markAllAsRead}>Mark all as read</div>
+        </div>
+
+        <div className="notifications">
+        {notificationData.map((notification, index) => (
+        <Notification
+          key={index}
+          data={notification}
+          onClick={() => handleNotificationClick(index)}
+        />
+      ))}
+        </div>
+      </main>
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/mikget66">Michael Anwar</a>.
+      </div>
+    </>
+  )
+}
+
+export default App
+
+// <img class="avatar" src="./profile-pictures/avatar-Mark Webber.webp" alt="">
---
+++ src/components/notification/Notification.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './notification.css'
+
+const Notification = ({data, onClick}) => {
+    return (
+        <article 
+            className={data.status === "not" ? "not_read" : ""}
+            onClick={onClick}
+        >
+            <img className="avatar" src={`profile-pictures/avatar-${data.name}.webp`} alt="" />
+
+            <div className="message">
+
+                <p>
+                    <span className="name">{`${data.name} `}</span>
+                    {`${data.msg} `}
+                    <span className='link'>{data.link}</span>
+                    <span className='post'>{data.post}</span>
+                    {data.status === "not" && (
+                    
+                       <span className="red"></span>
+                    
+                )}
+                </p>
+                <span className="time">{data.date}</span>
+                {data.dm && (
+                    <div className="content">
+                        {data.dm}
+                    </div>
+                )}
+            </div>
+            {data.img &&(
+                <img className='avatar' src={`image/${data.img}.webp`}/>
+            )
+
+            }
+        </article>
+    )
+}
+
+export default Notification
